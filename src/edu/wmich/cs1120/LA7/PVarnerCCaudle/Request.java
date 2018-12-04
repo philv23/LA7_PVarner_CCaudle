334f0319bef50c493230aa92a558314542f53e1e
philv <philv@10.80.31.100>
2018-12-03 19:20:52 -0500

    Phillip

--- src/edu/wmich/cs1120/LA7/PVarnerCCaudle/Request.java
+++ src/edu/wmich/cs1120/LA7/PVarnerCCaudle/Request.java
@@ -12,10 +12,12 @@ public class Request<T> implements Comparable<T>{
 	public Request(String studentName, String studentDept, String studentLevel,String courseDept, int courseNumber, double[][] GPA_Array) {}
 	// Returns number of years to graduation (0 for seniors, 1 for juniors etc.). This is determined from the
 	// student’s level – senior, junior, etc.
-	public int yearsFromGraduation(String level) {}
+	public int yearsFromGraduation(String level) {
+		return 0;}
 	 
 	// Calculate the GPA for a particular student.
-	private double GPA_Cal(double[][] GPA_Array) {}
+	private double GPA_Cal(double[][] GPA_Array) {
+		return 0;}
 	 
 	// Getters for a student’s name and department, and the department and number of a course
 	
