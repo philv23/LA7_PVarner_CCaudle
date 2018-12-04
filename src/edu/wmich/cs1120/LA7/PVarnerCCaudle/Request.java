9c15254305d1e810a211c5d785dbcc4922440176
coreycaudle <coreycaudle@wireless34-135.wireless.wmich.edu>
2018-12-03 19:13:49 -0500

    corey

--- src/edu/wmich/cs1120/LA7/PVarnerCCaudle/Request.java
+++ src/edu/wmich/cs1120/LA7/PVarnerCCaudle/Request.java
@@ -1,5 +1,23 @@
 package edu.wmich.cs1120.LA7.PVarnerCCaudle;
 
-public class Request {
+public class Request<T> implements Comparable<T>{
+
+	@Override
+	public int compareTo(T o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Constructor
+	public Request(String studentName, String studentDept, String studentLevel,String courseDept, int courseNumber, double[][] GPA_Array) {}
+	// Returns number of years to graduation (0 for seniors, 1 for juniors etc.). This is determined from the
+	// student’s level – senior, junior, etc.
+	public int yearsFromGraduation(String level) {}
+	 
+	// Calculate the GPA for a particular student.
+	private double GPA_Cal(double[][] GPA_Array) {}
+	 
+	// Getters for a student’s name and department, and the department and number of a course
+	
 
 }
