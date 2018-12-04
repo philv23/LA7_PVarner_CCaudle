4e320fa3649ca544781ba7c62b40b7ec108a9427
coreycaudle <coreycaudle@wireless34-135.wireless.wmich.edu>
2018-12-03 19:04:31 -0500

    corey

---
+++ src/edu/wmich/cs1120/LA7/PVarnerCCaudle/LinkedList.java
@@ -0,0 +1,12 @@
+package edu.wmich.cs1120.LA7.PVarnerCCaudle;
+
+public class LinkedList {
+	// Determine if the LinkedList is empty.
+	public boolean isEmpty();
+	// Add object received to the linked list if he/she has the priority.
+	public void add(E item);
+	// get the object in specific position in the LinkedList used to print the enrolled student contents inside the Course class and return the name
+	public E get(int position);
+	// Return number of elements in the list.
+	public int size();
+}
