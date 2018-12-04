957cc90b2b19951f0787ed99f48cc71cf12fa4b0
coreycaudle <coreycaudle@wireless34-135.wireless.wmich.edu>
2018-12-03 19:04:43 -0500

    corey

--- src/edu/wmich/cs1120/LA7/PVarnerCCaudle/PriorityQueue.java
+++ src/edu/wmich/cs1120/LA7/PVarnerCCaudle/PriorityQueue.java
@@ -1,5 +1,12 @@
 package edu.wmich.cs1120.LA7.PVarnerCCaudle;
 
 public class PriorityQueue {
-
+	// Determine if the priority queue is empty.
+		public boolean isEmpty();
+		// Add object received to the priority queue taking into consideration the rules governing priority.
+		public void enqueue(E data);
+		// Remove the next object to be processed from the priority queue.
+		public E dequeue();
+		// Print the contents of the queue
+		public void Qprint();
 }
