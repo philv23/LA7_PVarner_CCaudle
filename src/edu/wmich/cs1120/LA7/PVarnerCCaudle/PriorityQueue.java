56b9a4d7aa45372e5eaf771b5b20e78ee4f5869c
philv <philv@10.80.37.40>
2018-12-04 11:56:07 -0500

    Phillip

--- src/edu/wmich/cs1120/LA7/PVarnerCCaudle/PriorityQueue.java
+++ src/edu/wmich/cs1120/LA7/PVarnerCCaudle/PriorityQueue.java
@@ -1,13 +1,23 @@
 package edu.wmich.cs1120.LA7.PVarnerCCaudle;
 
-public class PriorityQueue {
+public class PriorityQueue<E> {
 	
-	// Determine if the priority queue is empty.
-		public boolean isEmpty();
+	    // Determine if the priority queue is empty.
+		public boolean isEmpty() {
+			return true;
+			
+		}
 		// Add object received to the priority queue taking into consideration the rules governing priority.
-		public void enqueue(E data);
+		public void enqueue(E data) {
+			
+		}
 		// Remove the next object to be processed from the priority queue.
-		public E dequeue();
+		public E dequeue() {
+			return null;
+			
+		}
 		// Print the contents of the queue
-		public void Qprint();
+		public void Qprint() {
+			
+		}
 }
