c85b8e9f26213a45c72edb5ea514e824f26fd0bd
coreycaudle <coreycaudle@wireless34-135.wireless.wmich.edu>
2018-12-04 11:53:54 -0500

    corey

--- src/edu/wmich/cs1120/LA7/PVarnerCCaudle/Controller.java
+++ src/edu/wmich/cs1120/LA7/PVarnerCCaudle/Controller.java
@@ -1,11 +1,19 @@
 package edu.wmich.cs1120.LA7.PVarnerCCaudle;
 
 public class Controller implements IController {
+	public LinkedList<Course> courses;
+	public PriorityQueue<Request> requestQueue;
+	public String fileIn;
+	public String fileIn1;
+	
+	public Controller(PriorityQueue<Request> requestQueue, LinkedList<Course> courses, String fileIn, String fileIn1) {
+		
+	}
 
 	@Override
 	public void readCourseFile() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
