77e76852b80f92a8800479a473fc877a828e89a8
coreycaudle <coreycaudle@wireless34-135.wireless.wmich.edu>
2018-12-04 12:08:46 -0500

    corey

--- src/edu/wmich/cs1120/LA7/PVarnerCCaudle/Controller.java
+++ src/edu/wmich/cs1120/LA7/PVarnerCCaudle/Controller.java
@@ -1,18 +1,46 @@
 package edu.wmich.cs1120.LA7.PVarnerCCaudle;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Controller implements IController {
 	public LinkedList<Course> courses;
 	public PriorityQueue<Request> requestQueue;
 	public String fileIn;
 	public String fileIn1;
+	public String major;
+	public String courseNum;
+	public String cap;
 	
 	public Controller(PriorityQueue<Request> requestQueue, LinkedList<Course> courses, String fileIn, String fileIn1) {
-		
+		this.courses = courses;
+		this.requestQueue = requestQueue;
+		this.fileIn = fileIn;
+		this.fileIn1 = fileIn1;
 	}
 
 	@Override
 	public void readCourseFile() {
 		// TODO Auto-generated method stub
+		String[] array = new String[3];
+		
+		File file = new File(fileIn);
+		try {
+			Scanner scanner = new Scanner(file);
+			while(scanner.hasNextLine()) {
+				array = scanner.toString().split(",");
+				major = array[0];
+				courseNum = array[1];
+				cap = array[2];
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
